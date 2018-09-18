ccdeff16f166b2224ca21fcc58115de03403c1dd
Kalsoom Malik <kalsoomalik09@gmail.com>
2018-09-18 00:07:28 -0400

    Update heroku configuration

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 worker: python app.py
-web: gunicorn --bind 0.0.0.0:5000 wsgi
+web: gunicorn --bind 0.0.0.0:{PORT} wsgi
--- app.py
+++ app.py
@@ -114,5 +114,5 @@ def samples_wfreq(sample):
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get('PORT',5000))
+    port = int(os.environ.get('PORT',5000))
     app.run(host='0.0.0.0', port=5000)
