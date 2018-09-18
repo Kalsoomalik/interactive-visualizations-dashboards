7e22d35615b188e5caab24ebe364b0ee65f7e584
Kalsoom Malik <kalsoomalik09@gmail.com>
2018-09-17 23:05:34 -0400

    updated

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def samples_wfreq(sample):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT',5000))
+    app.run()
